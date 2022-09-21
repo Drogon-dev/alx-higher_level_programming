976a978fd0f3cff4588e141fe855be50dc5da34c
Drogon-dev <69829036+Drogon-dev@users.noreply.github.com>
2022-09-21 04:54:43 +0100

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
-    for j in range(10):
-        if i >= j:
-            continue
-        elif i == 8 and j == 9:
+    for j in range(i + 1, 10):
+        if i == 8:
             print("{}{}".format(i, j))
-	else:
+        else:
             print("{}{}".format(i, j), end=", ")
